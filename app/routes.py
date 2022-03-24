292ff491ed342b046568598f68449c562b1ee476
xiyuanzhou <99446193+xiyuanzhou@users.noreply.github.com>
2022-03-24 00:25:51 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,13 @@ from flask import render_template, request
 city_names = ['Paris','London', 'Rome', 'Tahiti']
 name = 'Lisa'
 
-@myobj.route('/')
+@myobj.route('/', methods=["GET","POST"])
 def home():
+    if request.method == "POST":
+        text = request.form['text']
+        return text
+
     return render_template('home.html',
                            name = name,
                            city_names = city_names,
                            )
-
-@myobj.route('/', methods=['POST'])
-def home_post():
-    text = request.form['text']
-
-    return text
