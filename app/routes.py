f8d623be0d3de37e34eae91eaaa1592f686da67f
xiyuanzhou <99446193+xiyuanzhou@users.noreply.github.com>
2022-03-24 14:00:17 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from flask import render_template, request
 
 city_names = ['Paris','London', 'Rome', 'Tahiti']
 name = 'Lisa'
+myobj.config['SECRET_KEY'] = '1234'
 
 @myobj.route('/', methods=["GET","POST"])
 def home():
